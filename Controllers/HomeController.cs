ff4b3a36e251a82bfb60f37396115438b99d6194
Parker Chen <archer102125220.2015@gmail.com>
2026-05-29 18:11:04 +0800

    feat: implement custom status code pages and add 404 error handling support

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,8 +17,11 @@ public class HomeController : Controller
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+    public IActionResult Error(int? statusCode = null)
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return View(new ErrorViewModel { 
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier,
+            StatusCode = statusCode
+        });
     }
 }
--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -3,6 +3,8 @@ namespace DotNetMvcWeb.Models;
 public class ErrorViewModel
 {
     public string? RequestId { get; set; }
+    
+    public int? StatusCode { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
